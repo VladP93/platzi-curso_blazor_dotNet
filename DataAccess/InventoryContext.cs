3cf879b2198a6e39e0286dfb16ec92afea1a320e
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2021-01-26 00:04:37 -0600

    DataAccess project added - migration

---
+++ DataAccess/InventoryContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class InventoryContext : DbContext
+    {
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<InputOutputEntity> InOuts { get; set; }
+        public DbSet<WarehouseEntity> Warehouses { get; set; }
+        public DbSet<StorageEntity> Storages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Server=DESKTOP-769GIM4\\SQLEXPRESS; Databse=InventoryBlazorDB; User Id=sa; Password=1234");
+            }
+        }
+    }
+}
