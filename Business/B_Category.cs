0578c450d8a58b0f1d735b73a6b589a09535a041
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2021-01-27 00:30:50 -0600

    Business layer for CRUD operations

---
+++ Business/B_Category.cs
@@ -0,0 +1,47 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess;
+using System.Linq;
+
+namespace Business
+{
+    public class B_Category
+    {
+        public static List<CategoryEntity> CategoryList()
+        {
+            using (var db = new InventoryContext())
+            {
+                return db.Categories.ToList();
+            }
+        }
+
+        public void CreateCategory(CategoryEntity oCategory)
+        {
+            using (var db = new InventoryContext())
+            {
+                db.Categories.Add(oCategory);
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(CategoryEntity oCategory)
+        {
+            using (var db = new InventoryContext())
+            {
+                db.Categories.Update(oCategory);
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteCategory(CategoryEntity oCategory)
+        {
+            using (var db = new InventoryContext())
+            {
+                db.Remove(oCategory);
+                db.SaveChanges();
+            }
+        }
+    }
+}
